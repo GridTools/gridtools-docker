52a54c49fe824ecddb5e6fddf0149229dc5bae2f
Hannes Vogt <hannes@havogt.de>
2020-03-31 11:08:31 +0200

    Add clang 10

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ for version in 7 8 9; do
 done
 
 # Clang
-for version in 7 8 9; do
+for version in 7 8 9 10; do
     docker build --build-arg REPOSITORY=$1 --build-arg CLANG_VERSION=$version -t $1:clang-$version clang
     docker build --build-arg REPOSITORY=$1 --build-arg BASE=clang-$version -t $1:test-clang-$version test
 done
@@ -22,7 +22,7 @@ docker build --build-arg REPOSITORY=$1 -t $1:cuda-10.1 cuda
 docker build --build-arg REPOSITORY=$1 --build-arg BASE=cuda-10.1 -t $1:test-cuda-10.1 test
 
 # Clang-CUDA
-for clang_version in 9; do
+for clang_version in 9 10; do
     docker build --build-arg REPOSITORY=$1 --build-arg CLANG_VERSION=$clang_version -t $1:clang-$clang_version-cuda-10.1 clang-cuda
     docker build --build-arg REPOSITORY=$1 --build-arg BASE=clang-$clang_version-cuda-10.1 -t $1:test-clang-$clang_version-cuda-10.1 test
 done
