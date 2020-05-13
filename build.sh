718bd1656a63091cfb855be8cc4f49b92c631fea
Hannes Vogt <hannes@havogt.de>
2020-05-13 10:59:52 +0200

    Add HPX

--- build.sh
+++ build.sh
@@ -32,3 +32,9 @@ for clang_version in 10; do
     docker build --build-arg REPOSITORY=$1 --build-arg CLANG_VERSION=$clang_version -t $1:clang-$clang_version-hip clang-hip
     docker build --build-arg REPOSITORY=$1 --build-arg BASE=clang-$clang_version-hip -t $1:test-clang-$clang_version-hip test
 done
+
+# GCC with HPX (not by default as it will increase docker build time significantly)
+for gcc_version in 9; do
+    docker build --build-arg REPOSITORY=$1 --build-arg GCC_VERSION=$gcc_version --build-arg HPX_TAG=master -t $1:gcc-$version-hpx gcc-hpx
+    docker build --build-arg REPOSITORY=$1 --build-arg BASE=gcc-$version-hpx -t $1:test-gcc-$version-hpx test
+done
