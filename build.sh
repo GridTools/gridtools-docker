07ba1794d73146ff3c1009b11e391ae417362014
Hannes Vogt <hannes@havogt.de>
2020-07-24 22:23:53 +0200

    Add atlas (#12)

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -e
 docker build -t $1:base base
 
 # GCC
-for version in 7 8 9; do
+for version in 7 8 9 10; do
     docker build --build-arg REPOSITORY=$1 --build-arg GCC_VERSION=$version -t $1:gcc-$version gcc
     docker build --build-arg REPOSITORY=$1 --build-arg BASE=gcc-$version -t $1:test-gcc-$version test
 done
@@ -28,13 +28,21 @@ for clang_version in 9 10; do
 done
 
 # HIP
-for clang_version in 10; do
-    docker build --build-arg REPOSITORY=$1 --build-arg CLANG_VERSION=$clang_version -t $1:clang-$clang_version-hip clang-hip
-    docker build --build-arg REPOSITORY=$1 --build-arg BASE=clang-$clang_version-hip -t $1:test-clang-$clang_version-hip test
-done
+# deactivated because of a broken dependency (old container image still available)
+#for clang_version in 10; do
+#    docker build --build-arg REPOSITORY=$1 --build-arg CLANG_VERSION=$clang_version -t $1:clang-$clang_version-hip clang-hip
+#    docker build --build-arg REPOSITORY=$1 --build-arg BASE=clang-$clang_version-hip -t $1:test-clang-$clang_version-hip test
+#done
 
 # GCC with HPX (not by default as it will increase docker build time significantly)
-for gcc_version in 9; do
+for gcc_version in 10; do
     docker build --build-arg REPOSITORY=$1 --build-arg GCC_VERSION=$gcc_version --build-arg HPX_TAG=master -t $1:gcc-$version-hpx gcc-hpx
     docker build --build-arg REPOSITORY=$1 --build-arg BASE=gcc-$version-hpx -t $1:test-gcc-$version-hpx test
+done
+
+# with atlas
+for compiler in "gcc"; do
+    for version in 9; do
+        docker build --build-arg REPOSITORY=$1 --build-arg COMPILER=$compiler --build-arg VERSION=$version -t $1:$compiler-$version-atlas atlas
+    done
 done
