b00a06016945f6f7617745310a95447ed60d9088
Hannes Vogt <hannes@havogt.de>
2020-08-24 13:10:53 +0200

    Update to Ubuntu 20.04 (#13)
    - clang 8 removed from building (as resources are not available for 20.04)
    - switched to CUDA 11.0
    
    Old images should still work with clang 8 and CUDA 10.1.

--- build.sh
+++ build.sh
@@ -12,19 +12,19 @@ for version in 8 9 10; do
 done
 
 # Clang
-for version in 8 9 10; do
+for version in 9 10; do
     docker build --build-arg REPOSITORY=$1 --build-arg CLANG_VERSION=$version -t $1:clang-$version clang
     docker build --build-arg REPOSITORY=$1 --build-arg BASE=clang-$version -t $1:test-clang-$version test
 done
 
 # CUDA
-docker build --build-arg REPOSITORY=$1 -t $1:cuda-10.1 cuda
-docker build --build-arg REPOSITORY=$1 --build-arg BASE=cuda-10.1 -t $1:test-cuda-10.1 test
+docker build --build-arg REPOSITORY=$1 -t $1:cuda-11.0 cuda
+docker build --build-arg REPOSITORY=$1 --build-arg BASE=cuda-11.0 -t $1:test-cuda-11.0 test
 
 # Clang-CUDA
 for clang_version in 9 10; do
-    docker build --build-arg REPOSITORY=$1 --build-arg CLANG_VERSION=$clang_version -t $1:clang-$clang_version-cuda-10.1 clang-cuda
-    docker build --build-arg REPOSITORY=$1 --build-arg BASE=clang-$clang_version-cuda-10.1 -t $1:test-clang-$clang_version-cuda-10.1 test
+    docker build --build-arg REPOSITORY=$1 --build-arg CLANG_VERSION=$clang_version -t $1:clang-$clang_version-cuda-11.0 clang-cuda
+    docker build --build-arg REPOSITORY=$1 --build-arg BASE=clang-$clang_version-cuda-11.0 -t $1:test-clang-$clang_version-cuda-11.0 test
 done
 
 # HIP
